d2d9c70d82b902501cd93f7b29b78d30cb48fe78
Amir <amirhejazi@Amirs-MacBook-Pro.local>
2022-09-29 23:43:55 -0700

    models

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
-# Create your models here.
+class products(models.Model):
+    title =  models.CharField(max_length=200)
+    price = models.FloatField()
+    discount_price = models.FloatField()
+    category = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.CharField(max_length=300)
