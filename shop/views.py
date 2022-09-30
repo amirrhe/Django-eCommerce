4425ccb05576456a90fa8cdb3535c39f596a7ce9
Amir <amirhejazi@Amirs-MacBook-Pro.local>
2022-09-30 01:47:29 -0700

    add search logic

--- shop/views.py
+++ shop/views.py
@@ -3,4 +3,7 @@ from .models import *
 
 def index(request):
     obj = products.objects.all()
+    item_name = request.GET.get('item_name')
+    if item_name != '' and item_name is not None :
+        obj = products.objects.filter(title__icontains=item_name)
     return render(request, 'shop/index.html', {'product_obj': obj})
